3d7539d6fdf05c9bc4e75a106d8898b724129f27
Pedro Alarcon <pedroa54@icloud.com>
2024-05-01 17:59:29 -0500

    completes take home

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
-if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
-
-
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+author_1 = Author("Carry Bradshaw")
+author_2 = Author("Nathaniel Hawthorne")
+magazine_1 = Magazine("Vogue", "Fashion")
+magazine_2 = Magazine("Skate", "Skate")
+magazine_3 = Magazine("Thrasher", "Skate")
